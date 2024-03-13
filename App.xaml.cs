a8073ab283db24c060d2aa6f4254e71474572dc5
Bastien De Macédo <bdm.yssac@hotmail.fr>
2024-03-13 15:03:44 +0100

    Beginning of the project. Added some XLM for the frontend of the application

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CCST_MANAGER
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
